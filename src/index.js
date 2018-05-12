da8139d934fe6feb69ab7db90980ed50ebed4c6f
Viral Solani <viral.solani@gmail.com>
2018-05-12 21:52:53 +0530

    Passing Dynamic data with props

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { render } from "react-dom";
 import App from "./components/App";
-import StorePicker from "./components/StorePicker";
+//import StorePicker from "./components/StorePicker";
 import "./css/style.css";
 
 render(<App />, document.querySelector("#main"));
