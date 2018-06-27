46d707b8e44d2b00fea9ea9797f8da48bca83760
Viral Solani <viral.solani@gmail.com>
2018-06-27 12:08:19 +0530

    Displaying Order State

--- src/components/App.js
+++ src/components/App.js
@@ -51,7 +51,9 @@ class App extends React.Component {
             ))}
           </ul>
         </div>
-        <Order />
+
+        <Order fishes={this.state.fishes} order={this.state.order} />
+        
         <Inventory
           addFish={this.addFish}
           loadSampleFishes={this.loadSampleFishes}
